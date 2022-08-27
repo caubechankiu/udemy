c8f0d834027bbb7f8d4a79acb475448cc3262f0d
Trinh The Vi <thevi@Trinhs-MacBook-Air.local>
2022-08-28 00:08:26 +0700

    update

--- client/components/oauth-callback.js
+++ client/components/oauth-callback.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 class OAuthCallback extends React.Component {
+    constructor(props) {
+        super(props)
+    }
     componentDidMount() {
-        localStorage.setItem("access-token", this.props.location.query.acess_token);
+        localStorage.setItem("access_token", this.props.location.query.acess_token);
         window.location.href = "/";
     }
     render() {
